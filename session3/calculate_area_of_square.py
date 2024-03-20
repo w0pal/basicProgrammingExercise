0e5032e2339fb92ef3a872e073625b1ae3672182
w0pal <mnaufalmaulana84@gmail.com>
2024-03-20 17:24:32 +0700

    add session4

---
+++ session3/calculate_area_of_square.py
@@ -0,0 +1,5 @@
+length = input('input the length')
+width = input('input the width')
+
+area = width * length
+print(area)
---
+++ session3/varables.py
@@ -0,0 +1,31 @@
+# string
+name = 'naufal'
+# integer
+age = 18
+# boolean
+isFemale = False
+
+print('='*32)
+
+print('my name = ', name)
+print('my age = ', age)
+print('i am female = ', isFemale)
+
+print('='*32)
+
+# int
+a = b = c = 1
+# changing int to str
+c = '2'
+# changing int to bool
+b = True
+
+
+print('a = ',a)
+print('type = ', type(a))
+print('b = ',b)
+print('type = ', type(b))
+print('c = ',c)
+print('type = ', type(c))
+
+print('='*32)
